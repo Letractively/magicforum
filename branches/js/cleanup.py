231ef3e78937d39c5cf5f8074976c63c537847fe
roberto.alsina <roberto.alsina@acf4657b-e416-3e15-f05e-b81804d66811>
2011-03-31 00:39:23 +0000

    starting a branch with more magic in it

---
+++ branches/js/cleanup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.6
+
+import disqusapi as disqus
+
+api = disqus.DisqusAPI(open("key").read().strip())
+for t in api.threads.list(forum='magicmisteryforum', limit=100):
+    api.threads.remove(thread=t['id'])
+for t in api.posts.list(forum='magicmisteryforum', limit=100):
+    api.posts.remove(post=t['id'])
+
+api.threads.create(forum="magicmisteryforum", title="Feel free to create threads or post comments here.")
