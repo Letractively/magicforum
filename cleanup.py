23f19e0739e6bf5cb8fdf2a8dce02be52a44af14
roberto.alsina <roberto.alsina@acf4657b-e416-3e15-f05e-b81804d66811>
2011-03-31 01:40:13 +0000

    port improvements from trunk

--- cleanup.py
+++ cleanup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python2.6
 
 import disqusapi as disqus
-
+shortname = 'forum2'
 api = disqus.DisqusAPI(open("key").read().strip())
-for t in api.threads.list(forum='magicmisteryforum', limit=100):
+for t in api.threads.list(forum=shortname, limit=100):
     api.threads.remove(thread=t['id'])
-for t in api.posts.list(forum='magicmisteryforum', limit=100):
+for t in api.posts.list(forum=shortname, limit=100):
     api.posts.remove(post=t['id'])
 
-api.threads.create(forum="magicmisteryforum", title="Feel free to create threads or post comments here.")
+t = "Feel free to create threads or post comments here."
+api.threads.create(forum=shortname, title=t, identifier=t)
